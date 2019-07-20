02a9cbd346be798dd997b415be045053884204d0
kelvin hernandez cabrera <khernandez@condorlabs.io>
2019-07-20 17:25:09 -0500

    routes

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 //Importaciones
-var express = require('express');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 //Iniciar Variables
-var app = express();
+const app = express();
 
 //Conexion de BD
 mongoose.connection.openUri('mongodb://localhost:27017/MAPLY', (err, res) => {
     if (err) throw err;
     console.log('BD MAPLY \x1b[34m%s\x1b[0m', 'Activa');
 });
-//Creacion de Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Done!!'
-    });
-});
 
 //Escucha de peticiones
 app.listen(3000, () => {
     console.log('Express Up, Online sobre \x1b[34m%s\x1b[0m', 'puerto 3000');
-})
+});
+
+// Load api routes
+app.use('/', routes);
+
+
+module.exports = app;
---
+++ routes.js
@@ -0,0 +1,17 @@
+//Importaciones
+const express = require('express');
+const router = express.Router();
+
+//Creacion de Rutas
+
+/* istanbul ignore next */
+router.get('/', function(req, res) {
+    res.status(200).json({ response: 'API-MAPLY is working properly.' });
+});
+
+//user
+const userControllers = require('./src/controllers/personalMedico');
+router.get('/user', userControllers.userPersonalMedico);
+
+
+module.exports = router;
