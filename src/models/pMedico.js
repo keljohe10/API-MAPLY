5928a6e3205731eecd43f5482302609e141f2040
Neal DeOro <nealdop@hotmail.es>
2019-07-22 22:50:10 -0500

    Modelo Personal Medico Actualizado

--- src/models/pMedico.js
+++ src/models/pMedico.js
@@ -5,10 +5,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var pMedicoSchema = new Schema({
+    id:{type: Number, required:true, unique},
     nombre: {type:String, required:[true, 'campo obligatorio']},
     apellido: {type:String, required:[true, 'campo obligatorio']},
     email:{type:String, unique, required:[true, 'campo obligatorio']},
-    fechanac:{type:String, required:[true, 'campo obligatorio']},
+    fechanac:{type:Date, required:[true, 'campo obligatorio']},
     genero:{type:String, required:[true, 'campo obligatorio']},
     password:{type:String, required:[true, 'campo obligatorio']},
     eps:{type:String, required:[true, 'campo obligatorio']},
