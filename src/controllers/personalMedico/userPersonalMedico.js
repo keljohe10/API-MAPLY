a534dcc696e9438f1ab0e71f500bdf97915e015f
kelvin hernandez cabrera <khernandez@condorlabs.io>
2019-08-03 17:26:52 -0500

    login
    # Conflicts:
    #	package-lock.json

--- src/controllers/personalMedico/userPersonalMedico.js
+++ src/controllers/personalMedico/userPersonalMedico.js
@@ -1,25 +1,23 @@
-const PMedico = require('../../models/personalMedico/pMedico');
-const bcrypt = require('bcrypt');
+const PMedico = require("../../models/personalMedico/pMedico");
+const bcrypt = require("bcrypt");
 
 async function userPersonalMedico(req, res, next) {
+  try {
+    const user = await PMedico.findOne({ email: "neal3@hotmail.com" });
+    console.log(req.usuario);
 
-    try {
-        const user = await PMedico.findOne({ email: 'kel@GMAIL.com' });
-
-        if (!bcrypt.compareSync('128919200', user.password)) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'Credenciales incorrectas - password',
-                errors: { message: 'Credenciales incorrectas' }
-            });
-        } else {
-            res.status(200).send('Done');
-        }
-    } catch (error) {
-        next(error);
+    if (!bcrypt.compareSync("128919201", user.password)) {
+      return res.status(400).json({
+        ok: false,
+        mensaje: "Credenciales incorrectas - password",
+        errors: { message: "Credenciales incorrectas" }
+      });
+    } else {
+      res.status(200).send("Done");
     }
-
-
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = [userPersonalMedico];
