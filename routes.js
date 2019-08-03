e274b2763ae7e29c7e35e99c1b623abf997676ea
kelvin hernandez cabrera <khernandez@condorlabs.io>
2019-08-03 17:26:52 -0500

    login

--- routes.js
+++ routes.js
@@ -1,19 +1,27 @@
 //Importaciones
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const mdAutenticacion = require("./src/middlewares/autenticacion");
 
 //Creacion de Rutas
 
 /* istanbul ignore next */
-router.get('/', function(req, res) {
-    res.status(200).json({ response: 'API-MAPLY is working properly.' });
+router.get("/", function(req, res) {
+  res.status(200).json({ response: "API-MAPLY is working properly." });
 });
 
 //user
-const userControllers = require('./src/controllers/personalMedico');
-router.get('/user', userControllers.userPersonalMedico);
-router.post('/user', userControllers.createPersonalMedico);
+const userControllers = require("./src/controllers/personalMedico");
+router.get(
+  "/user",
+  mdAutenticacion.verificaToken,
+  userControllers.userPersonalMedico
+);
+router.post("/user", userControllers.createPersonalMedico);
 
-
+//signUp
+const signUpControllers = require("./src/controllers/signUp");
+router.post("/login", signUpControllers.login);
 
 module.exports = router;
